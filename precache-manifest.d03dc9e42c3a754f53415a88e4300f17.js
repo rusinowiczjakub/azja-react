a03f15b0e3a7e0a3e0cb37d0994e162d5542d60d
Jakub Rusinowicz <jru@moleo.pl>
2019-02-08 13:38:24 +0100

    Updates

---
+++ precache-manifest.d03dc9e42c3a754f53415a88e4300f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6676ca7e032d3030d90a4091f02c8aa4",
+    "url": "/azja-react/static/media/header.6676ca7e.svg"
+  },
+  {
+    "revision": "7d47756ea55229ea9421",
+    "url": "/azja-react/static/js/runtime~main.7d47756e.js"
+  },
+  {
+    "revision": "032ddb707c5127021b49",
+    "url": "/azja-react/static/js/main.032ddb70.chunk.js"
+  },
+  {
+    "revision": "fbfae6a6b31ade67e53d",
+    "url": "/azja-react/static/js/1.fbfae6a6.chunk.js"
+  },
+  {
+    "revision": "032ddb707c5127021b49",
+    "url": "/azja-react/static/css/main.bd8355ea.chunk.css"
+  },
+  {
+    "revision": "07dcba2ca4a5e5fd6c127899373ae06d",
+    "url": "/azja-react/index.html"
+  }
+];
